4403b9705c74af72d1c1827a816bec3d908e1e35
Jonas Bergman <jobe0900@student.miun.se>
2015-05-07 18:48:07 +0200

    Added a basic test scenario for database connection.

---
+++ database/catchtest/DatabaseHandler_test.cc
@@ -0,0 +1,26 @@
+/*
+ * DatabaseHandler_test.cc
+ *
+ *  Created on: 2015-05-07
+ *      Author: Jonas Bergman
+ */
+
+#include "../../catchtest/catch.hpp"
+
+SCENARIO ("Communicate with database", "[database]")
+{
+	GIVEN ("a valid database configuration structure")
+	{
+		WHEN ("we try to connect to the database")
+		{
+			THEN ("we should not receive an exception")
+			{
+
+			}
+		}
+	}
+}
+
+
+
+
