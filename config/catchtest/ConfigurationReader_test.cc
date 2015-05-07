84a1aaf0929721ff04227a4bea97d85404f10fd9
Jonas Bergman <jobe0900@student.miun.se>
2015-05-07 13:38:17 +0200

    Testcase for missing data in configuration file.

--- config/catchtest/ConfigurationReader_test.cc
+++ config/catchtest/ConfigurationReader_test.cc
@@ -11,6 +11,7 @@
 SCENARIO ("Use ConfigurationReader to read configuration from json file",
 		"[json],[config]")
 {
+	//------------------------------------------------------------------------
 	GIVEN ("a filename to a valid configuration file")
 	{
 		std::string filename("config/catchtest/testsettings.json");
@@ -28,6 +29,24 @@ SCENARIO ("Use ConfigurationReader to read configuration from json file",
 				REQUIRE (db_config.mUsername == "jonas");
 				REQUIRE (db_config.mPassword == "jonas");
 				REQUIRE (db_config.mDatabase == "mikh_style");
+			}
+		}
+	}
+
+	//------------------------------------------------------------------------
+	GIVEN ("a filename to a configuration file with missing information")
+	{
+		std::string filename("config/catchtest/testsettings-missing-name.json");
+
+		WHEN ("asking for database configuration")
+		{
+			ConfigurationReader config_reader(filename);
+			DatabaseConfig db_config;
+
+			THEN ("we get an exception")
+			{
+				REQUIRE_THROWS_AS (config_reader.getDatabaseConfiguration(db_config),
+						boost::property_tree::ptree_error&);
 			}
 		}
 	}
