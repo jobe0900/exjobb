6475a6483b196f81b7c3296bae6048a4bef01882
Jonas Bergman <jobe0900@student.miun.se>
2015-05-07 12:03:01 +0200

    Changed formatting of test file

--- config/catchtest/ConfigurationReader_test.cc
+++ config/catchtest/ConfigurationReader_test.cc
@@ -7,11 +7,15 @@
 
 #include "../../catchtest/catch.hpp"
 
-SCENARIO ("Use ConfigurationReader to read configuration from json file", "[json],[config]") {
-
-	GIVEN ("a filename to a valid configuration file") {
-		WHEN ("asking for database configuration") {
-			THEN ("we get a  database configuration filled out") {
+SCENARIO ("Use ConfigurationReader to read configuration from json file",
+		"[json],[config]")
+{
+	GIVEN ("a filename to a valid configuration file")
+	{
+		WHEN ("asking for database configuration")
+		{
+			THEN ("we get a  database configuration filled out")
+			{
 			}
 		}
 	}
