24f8aa75f62f798477eea0e1cfdd47719ae3de1e
Jonas Bergman <jobe0900@student.miun.se>
2015-05-07 13:26:09 +0200

    First implentation of test case for ConfigurationReader

--- config/catchtest/ConfigurationReader_test.cc
+++ config/catchtest/ConfigurationReader_test.cc
@@ -6,16 +6,28 @@
  */
 
 #include "../../catchtest/catch.hpp"
+#include "../ConfigurationReader.h"
 
 SCENARIO ("Use ConfigurationReader to read configuration from json file",
 		"[json],[config]")
 {
 	GIVEN ("a filename to a valid configuration file")
 	{
+		std::string filename("settings.json");
+
 		WHEN ("asking for database configuration")
 		{
+			ConfigurationReader config_reader(filename);
+			DatabaseConfig db_config;
+			config_reader.getDatabaseConfiguration(db_config);
+
 			THEN ("we get a  database configuration filled out")
 			{
+				REQUIRE (db_config.mHostname == "127.0.0.1");
+				REQUIRE (db_config.mPort == 5432);
+				REQUIRE (db_config.mUsername == "jonas");
+				REQUIRE (db_config.mPassword == "jonas");
+				REQUIRE (db_config.mDatabase == "mikh_style");
 			}
 		}
 	}
