10dc89f3a2aad5be906a033c28d0f31bb3c0a274
Jonas Bergman <jobe0900@student.miun.se>
2015-05-07 11:51:13 +0200

    Setting up basic test case for ConfigurationReader

---
+++ config/catchtest/ConfigurationReader_test.cc
@@ -0,0 +1,21 @@
+/*
+ * ConfigurationReader_test.cc
+ *
+ *  Created on: 2015-05-07
+ *      Author: Jonas Bergman
+ */
+
+#include "../../catchtest/catch.hpp"
+
+SCENARIO ("Use ConfigurationReader to read configuration from json file", "[json],[config]") {
+
+	GIVEN ("a filename to a valid configuration file") {
+		WHEN ("asking for database configuration") {
+			THEN ("we get a  database configuration filled out") {
+			}
+		}
+	}
+}
+
+
+
