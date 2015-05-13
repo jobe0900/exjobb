cfcd7d601c5f32aef76de13ccc0a459cdfcd674c
Jonas Bergman <jobe0900@student.miun.se>
2015-05-13 08:56:34 +0200

    Added MapProvider, abstract base class for database and file access to map data.

---
+++ mapprovider/MapProvider.h
@@ -0,0 +1,76 @@
+/**  A one line description of the class.
+ *
+ * #include "GraphBuilder.h"
+ *
+ * @author Jonas Bergman
+ */
+
+#ifndef MAPPROVIDER_MAPPROVIDER_H_
+#define MAPPROVIDER_MAPPROVIDER_H_
+
+// SYSTEM INCLUDES
+//
+#include <map>
+
+// PROJECT INCLUDES
+//
+
+// LOCAL INCLUDES
+//
+#include "TopologyEdge.h"
+#include "TopologyTypes.h"
+#include "TopologyVertex.h"
+
+// FORWARD REFERENCES
+//
+
+/**
+ * Interface for getting map data from file or database
+ */
+class MapProvider
+{
+public:
+// LIFECYCLE
+
+    /** Default constructor.
+     */
+    MapProvider() = default;
+
+
+    /** Copy constructor.
+     *
+     * @param from The value to copy to this object.
+     */
+    MapProvider(const MapProvider& from) = delete;
+
+
+    /** Destructor.
+     */
+    virtual ~MapProvider(void) {}
+
+
+// OPERATORS
+// OPERATIONS
+    /** Get edges from the provider of map data.
+     * @param   rEdgeMap    Reference to a map with edge id => edge.
+     */
+    virtual void    getTopologyEdges(std::map<EdgeId, TopologyEdge>& rEdgeMap) = 0;
+
+    /** Get vertices from the provider of map data.
+     * @param   rVertexMap    Reference to a map with vertex id => vertex.
+     */
+    virtual void    getTopologyVertices(std::map<VertexId, TopologyVertex>& rVertexMap) = 0;
+// ACCESS
+// INQUIRY
+
+protected:
+private:
+};
+
+// INLINE METHODS
+//
+
+// EXTERNAL REFERENCES
+//
+
+#endif /* MAPPROVIDER_MAPPROVIDER_H_ */
