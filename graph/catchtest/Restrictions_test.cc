27c8053b9bf98f4f34512885591dc01dafb01c16
Jonas Bergman <jobe0900@student.miun.se>
2015-05-26 12:01:08 +0200

    Added some more tests for VehiclePropertyRestrictions.

--- graph/catchtest/Restrictions_test.cc
+++ graph/catchtest/Restrictions_test.cc
@@ -26,9 +26,44 @@ SCENARIO ("Adding and fetching restrictions", "[restrictions]")
 
             r.setVehiclePropertyRestricionForEdge(id, vp);
 
+            // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
             THEN ("we should get be able to get the property and check")
             {
                 REQUIRE (r.vehicleProperties(id).maxSpeed == max_speed);
+            }
+
+            // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+            THEN ("we should get be able to get reference to property and change")
+            {
+                Restrictions::VehicleProperties& r_vp = r.vehicleProperties(id);
+                r_vp.maxHeight = 10.0;
+                REQUIRE (r.vehicleProperties(id).maxHeight == Approx(10.0));
+            }
+
+            // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+            THEN ("it should be reported as having restriction for edge")
+            {
+                REQUIRE (r.hasVehiclePropertyRestriction(id));
+            }
+        }
+
+        //....................................................................
+        WHEN ("we try to fetch vehicle property restriction for unknown edge")
+        {
+            THEN ("we should get a RestrictionsException")
+            {
+                REQUIRE_THROWS_AS (
+                    r.vehicleProperties(567),
+                    RestrictionsException&);
+            }
+        }
+
+        //....................................................................
+        WHEN ("we query for vehicle property restriction for unknown edge")
+        {
+            THEN ("we should get a false")
+            {
+                REQUIRE_FALSE (r.hasVehiclePropertyRestriction(567));
             }
         }
     }
