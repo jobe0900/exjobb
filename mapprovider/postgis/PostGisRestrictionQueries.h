9345bac426300e4d2c5c7115bd3715b6c5dcbb60
Jonas Bergman <jobe0900@student.miun.se>
2015-05-31 10:12:02 +0200

    Refactoring of queries, creating methods for different parts of queries.

--- mapprovider/postgis/PostGisRestrictionQueries.h
+++ mapprovider/postgis/PostGisRestrictionQueries.h
@@ -9,6 +9,7 @@
 #define MAPPROVIDER_POSTGIS_POSTGISRESTRICTIONQUERIES_H_
 
 #include <string>
+#include <sstream>
 #include <vector>
 #include <pqxx/pqxx>
 
@@ -26,11 +27,19 @@ struct PostGisRestrictionQueries
         RESTRICTION_TYPE
     };
 
+    /** Query for restrictions based on Vehicle Properties.
+     * @param   rTrans          Transaction to perform query in.
+     * @param   rTopoEdgeTable  Name of table with topology edges.
+     * @param   rOsmEdgeTable   Name of table with OSM edges.
+     * @param   rSchemaName     Name of the topology schema.
+     * @result  pqxx::result of the query
+     * @throw   pqxx::pqxx_exception
+     */
     static pqxx::result     getVehiclePropertyEdgeRestrictions(
         pqxx::transaction_base& rTrans,
-        const std::string& rTopoEdgeTable,
-        const std::string& rOsmEdgeTable,
-        const std::string& rSchemaName)
+        const std::string&      rTopoEdgeTable,
+        const std::string&      rOsmEdgeTable,
+        const std::string&      rSchemaName)
     {
         return rTrans.exec(
             "SELECT     edge_id, "
@@ -62,7 +71,47 @@ struct PostGisRestrictionQueries
         );
     }
 
-    /** Drop and create the table 'turning_restrictions'. */
+    /** Query for general access restrictions.
+     * @param   rTrans          Transaction to perform query in.
+     * @param   rTopoEdgeTable  Name of table with topology edges.
+     * @param   rOsmEdgeTable   Name of table with OSM edges.
+     * @param   rSchemaName     Name of the topology schema.
+     * @result  pqxx::result of the query
+     * @throw   pqxx::pqxx_exception
+     */
+    static pqxx::result     getAccessRestrictions(
+        pqxx::transaction_base& rTrans,
+        const std::string&      rTopoEdgeTable,
+        const std::string&      rOsmEdgeTable,
+        const std::string&      rSchemaName)
+    {
+        std::vector<std::string> columns {
+            "access",
+            "barrier",
+            "disused",
+            "noexit",
+            "motorcar",
+            "goods",
+            "hgv",
+            "psv",
+            "lhv",
+            "motor_vehicle",
+            "vehicle"
+        };
+
+        return rTrans.exec(
+            startOfQuery(rTopoEdgeTable) +
+            queryColumns(columns) +
+            midOfQuery(rSchemaName, rOsmEdgeTable) +
+            notNullColumns(columns) +
+            endOfQuery()
+        );
+    }
+
+    /** Drop and create the table 'turning_restrictions'.
+     * @param   rTrans          Transaction to perform query in.
+     * @throw   pqxx::pqxx_exception
+     */
     static void             dropCreateTurningRestrictionsTable(
         pqxx::transaction_base& rTrans)
     {
@@ -81,6 +130,7 @@ struct PostGisRestrictionQueries
      * @param   rTrans          The transaction to execute within.
      * @param   rOsmEdgeTable   The name of the table with original osm edges.
      * @param   rTopoEdgeTable  The name of the table with topology edges.
+     * @throw   pqxx::pqxx_exception
      */
     static void             identifyTurningRestrictions(
         pqxx::transaction_base& rTrans,
@@ -93,13 +143,79 @@ struct PostGisRestrictionQueries
         );
     }
 
-    /** Get the restrictions from the 'turning_restrictions' table. */
+    /** Get the restrictions from the 'turning_restrictions' table.
+     * @param   rTrans          Transaction to perform query in.
+     * @return  pqxx::result    The turning restrictions.
+     * @throw   pqxx::pqxx_exception
+     */
     static pqxx::result     getTurningRestrictions(
         pqxx::transaction_base& rTrans)
     {
          return rTrans.exec(
             "SELECT * FROM turning_restrictions"
         );
+    }
+
+private:
+    static std::string startOfQuery(const std::string& rTopoEdgeTable)
+    {
+        return (
+            "SELECT     edge_id, "
+            //-- osm data about original edge
+            "           osm.* "
+            "FROM      " + rTopoEdgeTable +
+            " JOIN ( "
+            "   SELECT  element_id "
+        );
+    }
+
+    static std::string midOfQuery(
+        const std::string& rSchemaName,
+        const std::string& rOsmEdgeTable)
+    {
+        return (
+            "   FROM    " + rSchemaName + ".relation "
+            "   JOIN    " + rOsmEdgeTable +
+            "   ON      topogeo_id = (topo_geom).id "
+            "   WHERE   highway in " + OsmHighway::typesAsCommaSeparatedString()
+        );
+    }
+
+    static std::string endOfQuery()
+    {
+        return (
+            ") AS osm "
+            "ON edge_id = element_id "
+            "ORDER BY edge_id ASC;"
+        );
+    }
+
+    static std::string queryColumns(const std::vector<std::string>& rCols)
+    {
+        std::ostringstream oss;
+        for(const std::string& col : rCols)
+        {
+            oss << ", " << col;
+        }
+        return oss.str();
+    }
+
+    static std::string notNullColumns(const std::vector<std::string>& rCols)
+    {
+        std::ostringstream oss;
+        oss << " AND (";
+        size_t i = 0;
+        for(const std::string& col : rCols)
+        {
+            oss << col << " IS NOT NULL ";
+            if(i < (rCols.size() - 1))
+            {
+                oss << " OR ";
+            }
+            ++i;
+        }
+        oss << ") ";
+        return oss.str();
     }
 };
 
