35d7ce797c336a94bd56358381cc3c973c03b3d8
Jonas Bergman <jobe0900@student.miun.se>
2015-07-29 15:36:05 +0200

    Added EdgeRestriction class for use on single edges.

---
+++ graph/EdgeRestriction.cc
@@ -0,0 +1,455 @@
+/*
+ * EdgeRestriction.cc
+ *
+ *      Author: Jonas Bergman
+ */
+
+#include "EdgeRestriction.h"  // class implemented
+
+// STATIC INITIALIZATION
+/*static*/ double EdgeRestriction::VehicleProperties::DEFAULT_DIMENSION_MAX
+    = std::numeric_limits<double>::max();
+
+/*static*/ unsigned EdgeRestriction::VehicleProperties::DEFAULT_SPEED_MAX
+    = std::numeric_limits<unsigned>::max();
+
+/*static*/ unsigned EdgeRestriction::VehicleProperties::DEFAULT_SPEED_MIN
+    = 0;
+
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+//============================= LIFECYCLE ====================================
+EdgeRestriction::~EdgeRestriction()
+{
+    delete mpVehicleProperties;
+    delete mpGeneralAccess;
+    delete mpBarrier;
+    for(auto it : mVehicleTypeAccessMap)
+    {
+        delete it.second;
+    }
+    for(auto it : mTurningRestrictions)
+    {
+        delete it;
+    }
+}
+//============================= OPERATORS ====================================
+//============================= OPERATIONS ===================================
+void
+EdgeRestriction::setVehiclePropertyRestriction(
+    EdgeRestriction::VehicleProperties* pVehicleProperties)
+{
+    delete mpVehicleProperties;
+    mpVehicleProperties = pVehicleProperties;
+}
+
+void
+EdgeRestriction::setGeneralAccessRestriction(
+    OsmAccess*  pGeneralAccess)
+{
+    delete mpGeneralAccess;
+    mpGeneralAccess = pGeneralAccess;
+}
+
+void
+EdgeRestriction::setGeneralAccessRestriction(
+    OsmAccess::AccessType  generalAccessType)
+{
+    delete mpGeneralAccess;
+    mpGeneralAccess = new OsmAccess(generalAccessType);
+}
+
+void
+EdgeRestriction::addVehicleTypeAccessRestriction(
+    OsmVehicle::VehicleType  vehicleType,
+    OsmAccess*               pAccess)
+{
+    if(hasVehicleTypeAccessRestriction(vehicleType))
+    {
+        auto old_access = mVehicleTypeAccessMap.find(vehicleType);
+        delete old_access->second;
+        mVehicleTypeAccessMap.erase(vehicleType);
+    }
+    mVehicleTypeAccessMap.insert({vehicleType, pAccess});
+}
+
+void
+EdgeRestriction::addVehicleTypeAccessRestriction(
+    OsmVehicle::VehicleType  vehicleType,
+    OsmAccess::AccessType    accessType)
+{
+    addVehicleTypeAccessRestriction(vehicleType, new OsmAccess(accessType));
+//    if(hasVehicleTypeAccessRestriction(vehicleType))
+//    {
+//        auto old_access = mVehicleTypeAccessMap.find(vehicleType);
+//        delete old_access->second;
+//        mVehicleTypeAccessMap.erase(vehicleType);
+//    }
+//    mVehicleTypeAccessMap.insert({vehicleType, new OsmAccess(accessType)});
+}
+
+void
+EdgeRestriction::setBarrierRestriction(
+    OsmBarrier* pBarrier)
+{
+    delete mpBarrier;
+    mpBarrier = pBarrier;
+}
+
+void
+EdgeRestriction::setBarrierRestriction(
+    OsmBarrier::BarrierType barrierType)
+{
+    delete mpBarrier;
+    mpBarrier = new OsmBarrier(barrierType);
+}
+
+void
+EdgeRestriction::addTurningRestriction(
+    OsmTurningRestriction*   pTurningRestriction)
+{
+    mTurningRestrictions.push_back(pTurningRestriction);
+}
+
+void
+EdgeRestriction::setDisusedRestriction()
+{
+    mIsDisusedEdge = true;
+}
+
+void
+EdgeRestriction::setNoExitRestriction()
+{
+    mIsNoExitEdge = true;
+}
+//============================= ACESS      ===================================
+std::vector<EdgeRestriction::RestrictionType>
+EdgeRestriction::restrictionTypes() const
+{
+    std::vector<EdgeRestriction::RestrictionType> rest_types;
+
+    for(int i = EdgeRestriction::VEHICLE_PROPERTIES;
+        i < EdgeRestriction::NR_RESTRICTION_TYPES;
+        ++i)
+    {
+        RestrictionType type = static_cast<RestrictionType>(i);
+        if(hasRestriction(type))
+        {
+            rest_types.push_back(type);
+        }
+    }
+
+    return rest_types;
+}
+
+const EdgeRestriction::VehicleProperties&
+EdgeRestriction::vehicleProperties() const
+{
+    if(!hasVehiclePropertyRestriction())
+    {
+        throw RestrictionsException(
+            "Restrictions:vehicleProperties: no restriction for edge");
+    }
+    return *mpVehicleProperties;
+}
+
+EdgeRestriction::VehicleProperties&
+EdgeRestriction::vehicleProperties()
+{
+    if(!hasVehiclePropertyRestriction())
+    {
+        throw RestrictionsException(
+            "Restrictions:vehicleProperties: no restriction for edge");
+    }
+    return *mpVehicleProperties;
+//    return const_cast<EdgeRestriction::VehicleProperties&>(
+//        static_cast<const EdgeRestriction&>(*this).vehicleProperties() );
+}
+
+//const std::map<EdgeIdType, EdgeRestrictions::VehicleProperties>&
+//EdgeRestriction::vehicleProperties() const
+//{
+//    return mVehiclePropertiesMap;
+//}
+
+const OsmAccess&
+EdgeRestriction::generalAccess() const
+{
+    if(!hasGeneralAccessRestriction())
+    {
+        throw RestrictionsException(
+            "Restrictions:generalAccess: no restriction for edge");
+    }
+    return *mpGeneralAccess;
+}
+
+OsmAccess&
+EdgeRestriction::generalAccess()
+{
+    if(!hasGeneralAccessRestriction())
+    {
+        throw RestrictionsException(
+            "Restrictions:generalAccess: no restriction for edge");
+    }
+    return *mpGeneralAccess;
+//    return const_cast<OsmAccess&>(
+//        static_cast<const EdgeRestrictions&>(*this).generalAccess(edgeId) );
+}
+
+//const std::map<EdgeIdType, OsmAccess>&
+//EdgeRestrictions::generalAccess() const
+//{
+//    return mGeneralAccessMap;
+//}
+
+const OsmAccess&
+EdgeRestriction::vehicleTypeAccess(
+    OsmVehicle::VehicleType vehicleType) const
+{
+    if(!hasVehicleTypeAccessRestriction(vehicleType))
+    {
+        throw RestrictionsException(
+            "Restrictions:vehicleTypeAccess: no restriction for"
+            + " vehicle type " + OsmVehicle::toString(vehicleType))
+            + "  for edge";
+    }
+    return mVehicleTypeAccessMap.find(vehicleType)->second;
+}
+
+OsmAccess&
+EdgeRestriction::vehicleTypeAccess(
+    OsmVehicle::VehicleType vehicleType)
+{
+    return const_cast<OsmAccess&>
+        (static_cast<const EdgeRestriction&>
+            (*this).vehicleTypeAccess(vehicleType)
+        );
+}
+
+std::vector<OsmVehicle::VehicleType>
+EdgeRestriction::vehicleTypesWithRestrictions() const
+{
+    std::vector<OsmVehicle::VehicleType> types;
+
+    for( int i = 0; i < OsmVehicle::NR_VEHICLE_TYPES; ++i)
+    {
+        OsmVehicle::VehicleType type = static_cast<OsmVehicle::VehicleType>(i);
+
+        if(hasVehicleTypeAccessRestriction(type))
+        {
+            types.push_back(type);
+        }
+    }
+
+    return types;
+}
+
+//const std::map<EdgeIdType, std::map<OsmVehicle::VehicleType, OsmAccess> >&
+//EdgeRestrictions::vehicleTypeAccessEdges() const
+//{
+//    return mVehicleTypeAccessMap;
+//}
+
+const OsmBarrier&
+EdgeRestriction::barrier() const
+{
+    if(!hasBarrierRestriction())
+    {
+        throw RestrictionsException(
+            "Restrictions:barrier: no restriction for edge");
+    }
+    return *mpBarrier;
+}
+
+//const std::map<EdgeIdType, OsmBarrier>&
+//EdgeRestrictions::barriers() const
+//{
+//    return mBarrierMap;
+//}
+
+const std::vector<OsmTurningRestriction*>&
+EdgeRestriction::turningRestrictions() const
+{
+    if(!hasTurningRestriction())
+    {
+        throw RestrictionsException(
+            "Restriction:turningRestriction: no turning restriction for edge");
+    }
+    return mTurningRestrictions;
+}
+
+std::vector<EdgeIdType>
+EdgeRestriction::restrictedTargetEdges() const
+{
+    std::vector<EdgeIdType> restricted_targets;
+
+    try
+    {
+        const auto& r_vec = this->turningRestrictions();
+
+        for(const auto& restr : r_vec)
+        {
+            restricted_targets.push_back(restr->toEdgeId());
+        }
+    }
+    catch (RestrictionsException& re)
+    {
+        // never mind
+    }
+    return restricted_targets;
+}
+
+//const std::set<EdgeIdType>&
+//EdgeRestriction::disusedEdges() const
+//{
+//    return mDisusedEdges;
+//}
+//
+//const std::set<EdgeIdType>&
+//EdgeRestrictions::noExitEdges() const
+//{
+//    return mNoExitEdges;
+//}
+
+bool
+EdgeRestriction::isEdgeRestricted(
+        const VehicleConfig& rVehicleConfig,
+        const OsmBarrier::RestrictionsRule& rBarrierRule,
+        const OsmAccess::AccessRule& rAccessRule) const
+{
+    const auto& restriction_types = restrictionTypes();
+
+    bool is_restricted = false;
+    bool is_generally_restricted = false;
+    bool is_vehicle_banned = false;
+
+    for(const auto& r : restriction_types)
+    {
+        switch (r)
+        {
+            case EdgeRestrictions::DISUSED:
+                is_restricted = true; break;
+            case EdgeRestrictions::VEHICLE_PROPERTIES:
+                if(vehicleProperties().restrictsAccess(rVehicleConfig))
+                {
+                    is_restricted = true;
+                }
+                break;
+            case EdgeRestrictions::BARRIER:
+                if(barrier().restrictsAccess(rBarrierRule))
+                {
+                    is_restricted = true;
+                }
+                break;
+            case EdgeRestrictions::GENERAL_ACCESS:
+                if(!generalAccess().allowsAccess(rAccessRule))
+                {
+                    is_generally_restricted = true;
+                }
+                continue;
+            case EdgeRestrictions::VEHICLE_TYPE_ACCESS:
+                if(!vehicleTypeAccess(rVehicleConfig.category)
+                    .allowsAccess(rAccessRule))
+                {
+                    is_vehicle_banned = true;
+                }
+                continue;
+            default:
+                continue;
+        }
+    }
+
+    if(is_restricted
+        || (is_generally_restricted && is_vehicle_banned)
+        || is_vehicle_banned)
+    {
+        return true; // this edge should not be added.
+    }
+    return false;
+}
+
+//============================= INQUIRY    ===================================
+bool
+EdgeRestriction::hasRestriction(
+    EdgeRestriction::RestrictionType type) const
+{
+    switch (type)
+    {
+        case VEHICLE_PROPERTIES:
+            return hasVehiclePropertyRestriction(); break;
+        case GENERAL_ACCESS:
+            return hasGeneralAccessRestriction(); break;
+        case VEHICLE_TYPE_ACCESS:
+            return hasVehicleTypeAccessRestriction(); break;
+        case BARRIER:
+            return hasBarrierRestriction(); break;
+        case TURNING:
+            return hasTurningRestriction(); break;
+        case DISUSED:
+            return hasDisusedRestriction(); break;
+        case NO_EXIT:
+            return hasNoExitRestriction(); break;
+        default:
+            return false;
+    }
+}
+
+bool
+EdgeRestriction::hasVehiclePropertyRestriction() const
+{
+    return mpVehicleProperties != nullptr;
+}
+
+//bool
+//EdgeRestriction::hasGeneralAccessRestriction(EdgeIdType edgeId) const
+//{
+//    auto it = mGeneralAccessMap.find(edgeId);
+//    return (it != mGeneralAccessMap.end());
+//}
+//
+//bool
+//EdgeRestrictions::hasVehicleTypeAccessRestriction(EdgeIdType edgeId) const
+//{
+//    auto it = mVehicleTypeAccessMap.find(edgeId);
+//    return (it != mVehicleTypeAccessMap.end());
+//}
+
+bool
+EdgeRestriction::hasVehicleTypeAccessRestriction(
+    OsmVehicle::VehicleType vehicleType) const
+{
+    auto it = mVehicleTypeAccessMap.find(vehicleType);
+    if (it != mVehicleTypeAccessMap.end())
+    {
+        return true;
+    }
+    return false;
+}
+
+bool
+EdgeRestriction::hasBarrierRestriction() const
+{
+    return  mpBarrier != nullptr;
+}
+
+bool
+EdgeRestriction::hasTurningRestriction() const
+{
+    return mTurningRestrictions.size() > 0;
+}
+
+bool
+EdgeRestriction::hasDisusedRestriction() const
+{
+    return mIsDisusedEdge;
+}
+
+bool
+EdgeRestriction::hasNoExitRestriction() const
+{
+    return mIsNoExitEdge;
+}
+/////////////////////////////// PROTECTED  ///////////////////////////////////
+
+/////////////////////////////// PRIVATE    ///////////////////////////////////
+
+
---
+++ graph/EdgeRestriction.h
@@ -0,0 +1,369 @@
+/**  The EdgeRestriction class contains different restrictions for edges
+ * in the graph such as dimensions, access, turn restrictions.
+ *
+ * #include "EdgeRestriction.h"
+ *
+ * @author  Jonas Bergman
+ */
+
+#ifndef GRAPH_EDGERESTRICTION_H_
+#define GRAPH_EDGERESTRICTION_H_
+
+// SYSTEM INCLUDES
+//
+#include <limits>
+#include <map>
+#include <set>
+
+// PROJECT INCLUDES
+//
+
+// LOCAL INCLUDES
+//
+#include "RestrictionsException.h"
+#include "../osm/OsmAccess.h"
+#include "../osm/OsmBarrier.h"
+#include "../osm/OsmTurningRestriction.h"
+#include "../osm/OsmVehicle.h"
+#include "Edge.h"
+#include "Vertex.h"
+#include "../config/VehicleConfig.h"
+
+// FORWARD REFERENCES
+//
+
+/**
+ * EdgeRestriction are:
+ * - vehicle properties
+ * - General access to an edge
+ * - vehicle type specific access
+ * - barriers
+ * - turn restrictions
+ * - disused roads
+ * - no-exit roads
+ */
+class EdgeRestriction
+{
+public:
+// TYPES
+    /** EdgeRestrictions on Vehicles to travel an Edge.
+     *  Dimensions in meters.
+     *  Speed in km/h
+     */
+    struct VehicleProperties
+    {
+        static double DEFAULT_DIMENSION_MAX;
+        // = {std::numeric_limits<double>::max()};
+
+        static unsigned DEFAULT_SPEED_MAX;
+        // = {std::numeric_limits<unsigned>::max()};
+
+        static unsigned DEFAULT_SPEED_MIN;
+        // = {0};
+
+        double      maxHeight   {DEFAULT_DIMENSION_MAX};
+        double      maxLength   {DEFAULT_DIMENSION_MAX};
+        double      maxWeight   {DEFAULT_DIMENSION_MAX};
+        double      maxWidth    {DEFAULT_DIMENSION_MAX};
+
+        unsigned    maxSpeed    {DEFAULT_SPEED_MAX};
+        unsigned    minSpeed    {DEFAULT_SPEED_MIN};
+
+        /** Look if the vehicle properties restricts
+         * vehicle with given configuration.
+         */
+        bool    restrictsAccess(const VehicleConfig& rVehicleConfig) const
+        {
+            return (maxHeight <= rVehicleConfig.height)
+                || (maxLength <= rVehicleConfig.length)
+                || (maxWeight <= rVehicleConfig.weight)
+                || (maxWidth  <= rVehicleConfig.width)
+                || (minSpeed  >= rVehicleConfig.maxspeed);
+        }
+    };
+
+    enum RestrictionType
+    {
+        VEHICLE_PROPERTIES,
+        GENERAL_ACCESS,
+        VEHICLE_TYPE_ACCESS,
+        BARRIER,
+        TURNING,
+        DISUSED,
+        NO_EXIT,
+
+        NR_RESTRICTION_TYPES
+    };
+
+// LIFECYCLE
+
+    /** Default constructor.
+     */
+    EdgeRestriction() = default;
+
+
+//    /** Copy constructor.
+//     *
+//     * @param from The value to copy to this object.
+//     */
+//    EdgeRestriction(const EdgeRestriction& from) = default;
+
+
+    /** Destructor.
+     */
+    ~EdgeRestriction();
+
+
+// OPERATORS
+// OPERATIONS
+    /** Set vehicle properties for the specified edge.
+     * Replacing any existing properties with the new ones.
+     * @param   pVehicleProperties  The properties to install for the edge.
+     */
+    void                setVehiclePropertyRestriction(
+                            VehicleProperties* pVehicleProperties);
+
+    /** Set access restrictions for this edge, that is restrictions for all.
+     * @param   pGeneralAccess  The access to set.
+     */
+    void                setGeneralAccessRestriction(
+                            OsmAccess*  pGeneralAccess);
+
+    /** Set access restrictions for this edge, that is restrictions for all.
+     * @param   generalAccessType  The access type to set.
+     */
+    void                setGeneralAccessRestriction(
+                            OsmAccess::AccessType  generalAccessType);
+
+    /** Set access restrictions for edge based on vehicle type.
+     * There can be several vehicle restrictions for each edge.
+     * @param   vehicleType The type of vehicle to restrict on the edge.
+     * @param   pAccess     The access restriction for that vehicle type
+     *                      on this edge.
+     */
+    void                addVehicleTypeAccessRestriction(
+                            OsmVehicle::VehicleType  vehicleType,
+                            OsmAccess*               pAccess);
+
+    /** Set access restrictions for edge based on vehicle type.
+     * There can be several vehicle restrictions for each edge.
+     * @param   vehicleType The type of vehicle to restrict on the edge.
+     * @param   accessType  The access restriction for that vehicle type
+     *                      on this edge.
+     */
+    void                addVehicleTypeAccessRestriction(
+                            OsmVehicle::VehicleType  vehicleType,
+                            OsmAccess::AccessType    accessType);
+
+    /** Set barrier restricting this edge.
+     * @param   pBarrier        The barrier to set.
+     */
+    void                setBarrierRestriction(
+                            OsmBarrier* pBarrier);
+
+    /** Set barrier restricting this edge.
+     * @param   barrierType    The barrier type to set.
+     */
+    void                setBarrierRestriction(
+                            OsmBarrier::BarrierType barrierType);
+
+    /** Add turning restrictions from this edge.
+     * Actually just adds the restriction without checking if there already is
+     * a restriction between those two edges.
+     * @param   pTurningRestriction  The turning restriction to set.
+     */
+    void                addTurningRestriction(
+                            OsmTurningRestriction* pTurningRestriction);
+
+    /** Set disused flag on this edge.
+     */
+    void                setDisusedRestriction();
+
+    /** Set no exit flag on this edge.
+     * @param   edgeId  The id of the edge to apply restrictions to.
+     */
+    void                setNoExitRestriction();
+
+// ACCESS
+    /** Get which kinds of restrictions this edge has.
+     * @return  A vector with all types of restrictions.
+     */
+    std::vector<RestrictionType>
+                        restrictionTypes() const;
+
+    /** Try to fetch the vehicle property restrictions for an Edge.
+     * @return  The Vehicle properties
+     * @throw   RestrictionException if no entry exists for Edge.
+     */
+    const VehicleProperties&
+                        vehicleProperties() const;
+    VehicleProperties&  vehicleProperties();
+
+//    /** Get the map of all VehicleProperty Restrictions.
+//     * @return  Map of all property restrictions.
+//     */
+//    const std::map<EdgeIdType, VehicleProperties>&
+//                        vehicleProperties() const;
+
+    /** Try to fetch the general access restrictions for this edge.
+     * @param   edgeId  The id of the edge.
+     * @return  reference to the OsmAccess object.
+     * @throw   RestrictionException if no entry exists for Edge.
+     */
+    const OsmAccess&    generalAccess() const;
+    OsmAccess&          generalAccess();
+
+//    /** Get all the general access restrictions.
+//     * @return map of edges with barriers.
+//     */
+//    const std::map<EdgeIdType, OsmAccess>&
+//                        generalAccess() const;
+
+    /** Try to fetch the vehicle type specific access restrictions for this edge.
+     * @param   vehiceltType    The type of Vehicle to get access restriction
+     * @return  reference to the OsmAccess object.
+     * @throw   RestrictionException if no entry exists for Edge.
+     */
+    const OsmAccess&    vehicleTypeAccess(
+                            OsmVehicle::VehicleType vehicleType) const;
+    OsmAccess&          vehicleTypeAccess(
+                            OsmVehicle::VehicleType vehicleType);
+
+    /** Get a list of the types of vehicles with restrictions on this edge.
+     * @return  a Vector with restriction types.
+     */
+    std::vector<OsmVehicle::VehicleType>
+                        vehicleTypesWithRestrictions() const;
+
+//    /**
+//     * @return Get the map of vehicle type restrictions for edges.
+//     */
+//    const std::map<EdgeIdType, std::map<OsmVehicle::VehicleType, OsmAccess> >&
+//                        vehicleTypeAccessEdges() const;
+
+    /** Fetch the barrier restricting this edge.
+     * @return  reference to a OsmBarrier object.
+     * @throw   RestrictionException if no entry exists for this Edge.
+     */
+    const OsmBarrier&   barrier() const;
+
+//    /** Get all the barrier restrictions.
+//     * @return map of edges with barriers.
+//     */
+//    const std::map<EdgeIdType, OsmBarrier>&
+//                        barriers() const;
+
+    /** Get a list of the turning restrictions from this edge.
+     * @return  a Vector with turning restrictions.
+     * @throw   RestrictionException if edge has no turning restrictions.
+     */
+    const std::vector<OsmTurningRestriction*>&
+                        turningRestrictions() const;
+
+    /** Get a list of all edge id's to which travel from edge is not allowed.
+     * @return  A vector of edgeIds to which travel is not allowed.
+     */
+    std::vector<EdgeIdType>
+                        restrictedTargetEdges() const;
+
+//    /** Get all edges marked as disused.
+//     * @return  The set of disused edges (ids).
+//     */
+//    const std::set<EdgeIdType>&
+//                        disusedEdges() const;
+//
+//    /** Get all edges marked as 'noexit'.
+//     * @return  The set of 'noexit' edges (ids).
+//     */
+//    const std::set<EdgeIdType>&
+//                        noExitEdges() const;
+// INQUIRY
+    /** Ask if an Edge has restriction of a certain type.
+     * @param   restrictionType     The type of restriction
+     * @return  true if there is a restriction of that type, false if not.
+     */
+    bool                hasRestriction(
+                            RestrictionType restrictionType) const;
+    /**
+     * @return true if there is a VehicleProperty restriction for edge.
+     */
+    bool                hasVehiclePropertyRestriction() const;
+
+    /**
+     * @return true if there is a General Access restriction for the edge.
+     */
+    bool                hasGeneralAccessRestriction() const;
+
+    /**
+     * @return true if there are any Vehicle Type Access restrictions for the edge.
+     */
+    bool                hasVehicleTypeAccessRestriction() const;
+
+    /**
+     * @return true if there are Vehicle Type Access restrictions for the edge
+     *         for that specific type of vehicle.
+     */
+    bool                hasVehicleTypeAccessRestriction(
+                            OsmVehicle::VehicleType vehicleType) const;
+
+    /**
+     * @return true if there are any barriers restricting access to the edge.
+     */
+    bool                hasBarrierRestriction() const;
+
+    /**
+     * @return true if there are any turning restrictions traveling from edge.
+     */
+    bool                hasTurningRestriction() const;
+
+    /**
+     * @return true if the edge is 'disused'.
+     */
+    bool                hasDisusedRestriction() const;
+
+    /**
+     * @return true if the edge has no exit.
+     */
+    bool                hasNoExitRestriction() const;
+
+    /** Check the restrictions for an edge.
+     * @param   rVehicleConfig  Configuration for the current vehicle.
+     * @param   rBarrierRule    Rules for which Barrier types restricts access.
+     * @param   rAccessRule     Rules for which Access types restricts access.
+     * @return  true if access is allowed, false if access restricted
+     */
+    bool                isEdgeRestricted(
+        const VehicleConfig& rVehicleConfig,
+        const OsmBarrier::RestrictionsRule& rBarrierRule,
+        const OsmAccess::AccessRule& rAccessRule) const;
+
+protected:
+private:
+    VehicleProperties*      mpVehicleProperties {nullptr};
+    OsmAccess*              mpGeneralAccess {nullptr};
+    std::map<OsmVehicle::VehicleType,  OsmAccess*>
+                            mVehicleTypeAccessMap;
+    OsmBarrier*             mpBarrier {nullptr};
+    std::vector<OsmTurningRestriction*>
+                            mTurningRestrictions;
+    bool                    mIsDisusedEdge {false};
+    bool                    mIsNoExitEdge {false};
+//    std::map<EdgeIdType, VehicleProperties>         mVehiclePropertiesMap;
+//    std::map<EdgeIdType, OsmAccess>                 mGeneralAccessMap;
+//    std::map<EdgeIdType, std::map<OsmVehicle::VehicleType, OsmAccess> >
+//                                                    mVehicleTypeAccessMap;
+//    std::map<EdgeIdType, OsmBarrier>                mBarrierMap;
+    // from edge id => turning restriction to several target edges
+//    std::map<EdgeIdType, std::vector<OsmTurningRestriction> >
+//                                                    mTurningRestrictionsMap;
+//    std::set<EdgeIdType>                            mDisusedEdges;
+//    std::set<EdgeIdType>                            mNoExitEdges;
+};
+
+// INLINE METHODS
+//
+
+// EXTERNAL REFERENCES
+//
+
+#endif /* GRAPH_EDGERESTRICTION_H_ */
