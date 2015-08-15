acb74f3aa0a3e155f8a0013b49b443d85c76a0dd
Jonas Bergman <jobe0900@student.miun.se>
2015-08-15 09:51:56 +0200

    Updated test for LGU with print out of lines.

--- lgu/catchtest/LineGraphUtility_test.cc
+++ lgu/catchtest/LineGraphUtility_test.cc
@@ -17,7 +17,7 @@ SCENARIO ("LineGraphUtility construction", "[lgu][construction]")
     {
         GIVEN ("a valid config file set up to use jsontest as provider")
         {
-            std::string config_file("lgu/catchtest/jsontest-settings.json");
+		    std::string config_file("catchtest/testsettings/mikh_restr_0617-testsettings.json");
 
             //................................................................
             WHEN ("creating a LineGraphUtility")
@@ -52,13 +52,38 @@ SCENARIO ("LineGraphUtility operation", "[lgu][operation]")
 
             WHEN ("asking for a LineGraph")
             {
-                Graph::LineGraphType* p_lg {nullptr};
-                p_lg = lgu.getLineGraph();
+                Graph::LineGraphType* p_lg = lgu.getLineGraph();
 
                 THEN ("we should have a line graph")
                 {
                     REQUIRE (p_lg != nullptr);
                     REQUIRE (boost::num_edges(*p_lg) > 0);
+                }
+
+                THEN ("we should be able to print info for lines")
+                {
+                    int i = 0;
+                    for(auto it = boost::edges(*p_lg);
+                        (i < 10) && (it.first != it.second) ;
+                        ++it.first, ++i)
+                    {
+                        const auto& line = *(it.first);
+                        NodeIdType lg_source_id =
+                            boost::get(&LineGraphLine::lgSourceNodeId, *p_lg, line);
+                        NodeIdType lg_target_id =
+                            boost::get(&LineGraphLine::lgTargetNodeId, *p_lg, line);
+                        VertexIdType topo_via_vertex_id =
+                            boost::get(&LineGraphLine::topoViaVertexId, *p_lg, line);
+                        double cost =
+                            boost::get(&LineGraphLine::cost, *p_lg, line);
+
+                        INFO ("LINE:   lg_source_id: " << lg_source_id
+                            << ", lg_target_id: " << lg_target_id
+                            << ", topo_via_vertex_id: " << topo_via_vertex_id
+                            << ", cost: " << cost << "\n");
+                        REQUIRE (true);
+
+                    }
                 }
 
                 delete p_lg;
