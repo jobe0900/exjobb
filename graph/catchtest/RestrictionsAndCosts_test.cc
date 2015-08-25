f4aa1a91e35e2f6f1167c8e612dec38fd06aadc1
Jonas Bergman <jobe0900@student.miun.se>
2015-08-25 14:30:09 +0200

    Added test for barrier Lift Gate

--- graph/catchtest/RestrictionsAndCosts_test.cc
+++ graph/catchtest/RestrictionsAndCosts_test.cc
@@ -207,7 +207,7 @@ SCENARIO ("Building graph of Mikhailovsk with barrier block",
         Graph orig_graph(orig_topology, orig_config);
 
         // ===================================================================
-        GIVEN ("Configuration to build a Graph with barrier block ")
+        GIVEN ("Configuration that restricts barrier block ")
         {
             std::string config_file("catchtest/testsettings/"
                 "restrictions/mikhailovsk-barrier_block.json");
@@ -294,7 +294,7 @@ SCENARIO ("Building graph of Partille with barrier block",
         Graph orig_graph(orig_topology, orig_config);
 
         // ===================================================================
-        GIVEN ("Configuration to build a Graph with barrier block ")
+        GIVEN ("Configuration that restricts barrier block ")
         {
             std::string config_file("catchtest/testsettings/"
                 "restrictions/partille-barrier_block.json");
@@ -385,7 +385,7 @@ SCENARIO ("Building graph of Mikhailovsk with barrier bollard",
         Graph orig_graph(orig_topology, orig_config);
 
         // ===================================================================
-        GIVEN ("Configuration to build a Graph with barrier bollard ")
+        GIVEN ("Configuration that restricts barrier bollard ")
         {
             std::string config_file("catchtest/testsettings/"
                 "restrictions/mikhailovsk-barrier_bollard.json");
@@ -472,7 +472,7 @@ SCENARIO ("Building graph of Partille with barrier bollard",
         Graph orig_graph(orig_topology, orig_config);
 
         // ===================================================================
-        GIVEN ("Configuration to build a Graph with barrier bollard ")
+        GIVEN ("Configuration that restricts barrier bollard ")
         {
             std::string config_file("catchtest/testsettings/"
                 "restrictions/partille-barrier_bollard.json");
@@ -525,6 +525,179 @@ SCENARIO ("Building graph of Partille with barrier bollard",
                     INFO ("  Original # Lines:    " << orig_graph.nrLines());
                     INFO ("Restricted # Lines:    " << graph.nrLines());
                     REQUIRE ((orig_graph.nrLines() - 8) == graph.nrLines());
+                }
+            }
+        }
+    }
+    catch (ConfigurationException& e)
+    {
+        INFO(e.what());
+        REQUIRE (false);    // force output of error and failure
+    }
+    catch (MapProviderException& dbe)
+    {
+        INFO(dbe.what());
+        REQUIRE (false);    // force output of error and failure
+
+    }
+}
+
+// BARRIER LIFT GATE /////////////////////////////////////////////////////////
+
+SCENARIO ("Building graph of Mikhailovsk with barrier lift gate",
+    "[graph][r_and_c][lift_gate][mikhailovsk]")
+{
+    // block on node 1706164751 on way 158421713
+    try
+    {
+        std::string orig_config_file("catchtest/testsettings/"
+            "restrictions/mikhailovsk-original.json");
+        ConfigurationReader orig_config_reader(orig_config_file);
+        Configuration orig_config;
+        orig_config_reader.fillConfiguration(orig_config);
+        PostGisProvider orig_pgp(orig_config);
+        Topology orig_topology;
+        orig_pgp.getTopology(orig_topology);
+        orig_pgp.setRestrictionsAndCosts(orig_topology);
+        Graph orig_graph(orig_topology, orig_config);
+
+        // ===================================================================
+        GIVEN ("Configuration that does not restrict barrier lift gate ")
+        {
+            std::string config_file("catchtest/testsettings/"
+                "restrictions/mikhailovsk-barrier_lift_gate.json");
+            ConfigurationReader config_reader(config_file);
+            Configuration config;
+            config_reader.fillConfiguration(config);
+
+            PostGisProvider pgp(config);
+
+            Topology topology;
+            pgp.getTopology(topology);
+            pgp.setRestrictionsAndCosts(topology);
+
+            Graph graph(topology, config);
+
+            // ...............................................................
+            WHEN ("Comparing original to graph with barrier lift gate")
+            {
+
+                THEN ("there should be equally many vertices "
+                      "in original and restricted")
+                {
+                    INFO ("  Original # Vertices: " << orig_graph.nrVertices());
+                    INFO ("Restricted # Vertices: " << graph.nrVertices());
+                    REQUIRE (orig_graph.nrVertices() == graph.nrVertices());
+                }
+
+                THEN ("there should be equally many edges "
+                      "in original as in restricted")
+                {
+                    INFO ("  Original # Edges: " << orig_graph.nrEdges());
+                    INFO ("Restricted # Edges: " << graph.nrEdges());
+                    REQUIRE ((orig_graph.nrEdges()) == graph.nrEdges());
+                }
+
+                THEN ("there should be equally many nodes "
+                      "in original as in restricted")
+                {
+                    INFO ("  Original # Nodes:    " << orig_graph.nrNodes());
+                    INFO ("Restricted # Nodes:    " << graph.nrNodes());
+                    REQUIRE ((orig_graph.nrNodes()) == graph.nrNodes());
+                }
+
+                THEN ("there should be equally many lines "
+                      "in original as in restricted")
+                {
+                    INFO ("  Original # Lines:    " << orig_graph.nrLines());
+                    INFO ("Restricted # Lines:    " << graph.nrLines());
+                    REQUIRE ((orig_graph.nrLines()) == graph.nrLines());
+                }
+            }
+        }
+    }
+    catch (ConfigurationException& e)
+    {
+        INFO(e.what());
+        REQUIRE (false);    // force output of error and failure
+    }
+    catch (MapProviderException& dbe)
+    {
+        INFO(dbe.what());
+        REQUIRE (false);    // force output of error and failure
+
+    }
+}
+
+
+SCENARIO ("Building graph of Partille with barrier lift gate",
+    "[graph][r_and_c][lift_gate][partille]")
+{
+    // lift gate on node 249292683 on way 28050664
+    try
+    {
+        std::string orig_config_file("catchtest/testsettings/"
+            "restrictions/partille-original.json");
+        ConfigurationReader orig_config_reader(orig_config_file);
+        Configuration orig_config;
+        orig_config_reader.fillConfiguration(orig_config);
+        PostGisProvider orig_pgp(orig_config);
+        Topology orig_topology;
+        orig_pgp.getTopology(orig_topology);
+        orig_pgp.setRestrictionsAndCosts(orig_topology);
+        Graph orig_graph(orig_topology, orig_config);
+
+        // ===================================================================
+        GIVEN ("Configuration that does not restrict barrier lift gate ")
+        {
+            std::string config_file("catchtest/testsettings/"
+                "restrictions/partille-barrier_lift_gate.json");
+            ConfigurationReader config_reader(config_file);
+            Configuration config;
+            config_reader.fillConfiguration(config);
+
+            PostGisProvider pgp(config);
+
+            Topology topology;
+            pgp.getTopology(topology);
+            pgp.setRestrictionsAndCosts(topology);
+
+            Graph graph(topology, config);
+
+            // ...............................................................
+            WHEN ("Comparing original to graph with barrier lift gate")
+            {
+
+                THEN ("there should be equally many vertices "
+                      "in original and restricted")
+                {
+                    INFO ("  Original # Vertices: " << orig_graph.nrVertices());
+                    INFO ("Restricted # Vertices: " << graph.nrVertices());
+                    REQUIRE (orig_graph.nrVertices() == graph.nrVertices());
+                }
+
+                THEN ("there should be equally many edges "
+                      "in original as in restricted")
+                {
+                    INFO ("  Original # Edges: " << orig_graph.nrEdges());
+                    INFO ("Restricted # Edges: " << graph.nrEdges());
+                    REQUIRE ((orig_graph.nrEdges()) == graph.nrEdges());
+                }
+
+                THEN ("there should be equally many nodes "
+                      "in original as in restricted")
+                {
+                    INFO ("  Original # Nodes:    " << orig_graph.nrNodes());
+                    INFO ("Restricted # Nodes:    " << graph.nrNodes());
+                    REQUIRE ((orig_graph.nrNodes()) == graph.nrNodes());
+                }
+
+                THEN ("there should be equally many lines "
+                      "in original as in restricted")
+                {
+                    INFO ("  Original # Lines:    " << orig_graph.nrLines());
+                    INFO ("Restricted # Lines:    " << graph.nrLines());
+                    REQUIRE ((orig_graph.nrLines()) == graph.nrLines());
                 }
             }
         }
