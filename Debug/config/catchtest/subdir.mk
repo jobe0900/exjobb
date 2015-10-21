b97f24cd2e1053b2980dbd9c9b8d8dba2f9d390c
Jonas Bergman <jobe0900@student.miun.se>
2015-10-21 15:18:17 +0200

    Changed build settings.

--- Debug/config/catchtest/subdir.mk
+++ Debug/config/catchtest/subdir.mk
@@ -17,7 +17,7 @@ OBJS += \
 config/catchtest/%.o: ../config/catchtest/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -std=c++11 -DBOOST_LOG_DYN_LINK -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++11 -DBOOST_LOG_DYN_LINK -O0 -g3 -Wall -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Debug/osm/catchtest/subdir.mk
+++ Debug/osm/catchtest/subdir.mk
@@ -29,7 +29,7 @@ OBJS += \
 osm/catchtest/%.o: ../osm/catchtest/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -std=c++11 -DBOOST_LOG_DYN_LINK -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++11 -DBOOST_LOG_DYN_LINK -O0 -g3 -Wall -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
